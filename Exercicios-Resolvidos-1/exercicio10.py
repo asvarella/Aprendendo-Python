a36798634891b2142ff0b2bf7a6348ae738af1f0
asvarella <alice.varella@gmail.com>
2022-04-05 21:51:19 -0300

    ajuste de casas decimais

--- Exercicios-Resolvidos-1/exercicio10.py
+++ Exercicios-Resolvidos-1/exercicio10.py
@@ -3,7 +3,8 @@ Exercício 10:
 Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a
 quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante
 perde 10 minutos de vida a cada cigarro, calcule quantos dias de vida um fumante perderá. Exiba o
-total de dias.
+total de dias.
+
 '''
 
 numCigarros = int(input('Quantos cigarros você fuma por dia? '))
@@ -11,5 +12,5 @@ numAnos = float(input('Você fuma há quantos anos? '))
 
 diasPerdidos = (numAnos * 365 * numCigarros * 600)/86400
 
-print(f'Você perdeu um total de {diasPerdidos: .0f} dias de vida por fumar.')
+print(f'Você perdeu um total de {diasPerdidos: .1f} dias de vida por fumar.')
 
